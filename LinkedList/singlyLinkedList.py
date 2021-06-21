b73011c68df1f54bd74c0b58184d0a853c547b6d
Wubeshet Yimam <wubeane@gmail.com>
2021-06-21 19:42:29 +0300

    add delete operation methods

--- LinkedList/singlyLinkedList.py
+++ LinkedList/singlyLinkedList.py
@@ -10,6 +10,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # return the length of the linked list
+    def listLength(self):
+        currentNode = self.head
+        length = 0
+        while currentNode is not None:
+            currentNode = currentNode.next
+            length += 1
+        return length 
+
     # insert new Node at the end of the linked list 
     def insertEnd(self, newNode):
 
@@ -39,29 +48,90 @@ class LinkedList:
 
     # insert a new node between to nodes
     def insertAt(self, newNode, position):
-        
+        if position > self.listLength() or position < 1:
+            print("Invalid Position")
+            return
         if position == 1:
             self.insertHead(newNode)
             return
         i = 1
         currentNode = self.head    
+        previousNode = currentNode
         while i <= position:
             
             if i == position:
                 previousNode.next = newNode
                 newNode.next = currentNode
-                # followingNode = precedingNode.next
-                # precedingNode.next = newNode
-                # newNode.next = followingNode
-                # del followingNode
                 break
 
             previousNode = currentNode
             currentNode = currentNode.next
             i+=1
             
+    # delete the node at the end of the linked list
+    def deleteEnd(self):
+        # steps
+        # 1.Traverse until we the end of the list (when currentNode.next
+        # is None)
+        # 2. change previousNode.next to None - so that it lose connection 
+        # in the list
 
-    # prints the linked list
+        currentNode = self.head
+        if self.listLength() == 1:
+            self.head = None
+            return
+        previousNode = currentNode    
+        while True:
+            if currentNode.next is None:
+                # del currentNode - this operation only delete the reference to
+                # currentNode to the actual data in the memory.
+                previousNode.next = None
+                break
+            previousNode = currentNode
+            currentNode = currentNode.next
+      # prints the linked list
+    
+    # delete the head node
+    def deleteHead(self):
+        if self.listLength != 0:
+            previousNode = self.head
+            self.head = self.head.next
+            previousNode.next = None
+        else:
+            print("List is empty. Deletion failed!")
+        return        
+    # delete a node between two nodes
+    def deleteAt(self, position):
+        # steps
+        # 1. Traverse till the node to be deleted
+        # 2. preserve the details of the previous node
+        # 3. establish a connection betwee the previous node and the next node
+        # of the current node.
+        if position > self.listLength() or position < 1:
+            print("Invalid position")
+            return
+        if self.listLength == 0:
+            print("The LinkedList is empty")
+            return         
+        if position == 1:
+            self.deleteHead()
+            return
+        currentPosition = 1
+        currentNode = self.head
+        
+        while currentPosition < position:
+            previousNode = currentNode
+            currentPosition += 1
+            currentNode = currentNode.next
+        if currentNode.next is None:
+            self.deleteEnd()    
+            return
+        previousNode.next = currentNode.next
+        currentNode.next = None
+
+            
+
+    # print the linked list
     def printList(self):
         if self.head is None:
             print("list is empty")
@@ -72,6 +142,8 @@ class LinkedList:
                 break
             print(currentNode.data)
             currentNode = currentNode.next
+
+
             
 linkedList = LinkedList()
 
@@ -83,7 +155,9 @@ thirdNode = Node("kebe")
 linkedList.insertEnd(thirdNode)
 
 fourthNode = Node("lemma")
-linkedList.insertAt(fourthNode, 2)
+linkedList.insertAt(fourthNode, 3)
+linkedList.deleteAt(1)
 linkedList.printList()
+
 
 print("head: ",linkedList.head.data)
