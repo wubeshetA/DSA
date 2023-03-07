b015cf0bfaf5f61aede546d6a7271c9a48d92973
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-07 11:45:23 +0300

    Add recursive_counter.py

---
+++ recursive_counter.py
@@ -0,0 +1,14 @@
+"""A recursive function to count the number of items in a list"""
+
+def counter(lst):
+    
+    # Base case
+    if len(lst) == 0:
+        return 0
+    if len(lst) == 1:
+        return 1
+    return 1 + counter(lst[1:])
+    # Recursive case
+    
+lst = [1, 2, 3]
+print(counter(lst))
