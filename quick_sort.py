4fdf0022ff158ab970db40870ff73a8346df28cd
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-08 10:51:14 +0300

    Add quick sort implementation

---
+++ quick_sort.py
@@ -0,0 +1,36 @@
+"""Quick sort implementation.
+Using the Divide and Conqur method.
+"""
+
+
+def quick_sort(lst):
+
+    # Base case
+    if len(lst) < 2:
+        return lst
+    elif len(lst) == 2:
+        if lst[0] < lst[1]:
+            return lst
+        else:
+            lst[0], lst[1] = lst[1], lst[0]
+            return lst
+
+    # Recursive case
+    else:
+        # pick pivot
+        pivot = lst[0]
+
+        # find elements smaller and larger than the pivot
+        left = []
+        right = []
+        for elmt in lst[1:]:
+            if elmt <= pivot:
+                left += [elmt]
+            else:
+                right += [elmt]
+        return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+if __name__ == "__main__":
+    lst = [13, 23, 6, 3, 7]
+    print(quick_sort(lst))
