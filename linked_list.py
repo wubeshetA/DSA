0997d6926ff41b2464ab113b217db8b6e89ad7af
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-18 12:37:21 +0300

    Add insert_head and insert_tail methods

--- linked_list.py
+++ linked_list.py
@@ -22,14 +22,13 @@ class Node:
         """
         self.data = data # store the actual data
         self.next = None # points to the next node in the linear order
-        self.head = self.next
 
 class LinkedList:
     
     def __init__(self):
         self.head = None
         self.tail = None
-    def list_length(self, head):
+    def length(self, head):
         if type(head) == Node:
             counter = 1
             current = head
@@ -39,3 +38,52 @@ class LinkedList:
             return counter
         
     
+    def insert_head(self, head):
+        head.next = self.head
+        self.head = head
+        if self.length(self.head) == 1:
+            self.tail = head
+
+    def insert_tail(self, tail):
+        self.tail.next = tail
+        self.tail = tail
+        
+        
+    def insert_at(self, node, index):
+        pass
+    def get_at(self, index):
+        pass
+    def delete(self, index):
+        pass
+    def delete_head(self):
+        pass
+    def delete_tail(self):
+        pass
+    
+    def __str__(self) -> str:
+        current = self.head
+        list_data = []
+        # list_data.append(current.data)
+        while type(current) is Node:
+            list_data.append(current.data)
+            current = current.next
+            
+        return str(list_data)
+    
+    
+        
+        
+if __name__ == "__main__":
+    
+    node1 = Node(4)
+    node2 = Node(5)
+    node3 = Node(6)
+    
+    
+lst = LinkedList()
+lst.insert_head(node1)
+lst.insert_tail(node2)
+lst.insert_tail(node3)
+
+print(lst.length(node1))
+print(lst)
